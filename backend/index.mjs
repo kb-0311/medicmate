29fc937a296cf5091c73bb6f86e3c4674ae77e2b
Kanishka <kanishka.bansode@gmail.com>
2024-04-19 18:26:27 +0530

    backend init

---
+++ backend/index.mjs
@@ -0,0 +1,29 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hello world</h1>');
+// });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+
+server.listen(8000, () => {
+  console.log('server running at http://localhost:8000');
+});
