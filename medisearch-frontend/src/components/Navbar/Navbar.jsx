d164dfcc0363e7279d3e95b9f466bac5ac9afca6
yashharne <yashharne.ed@gmail.com>
2024-04-22 22:30:20 +0530

    navbar changes

--- medisearch-frontend/src/components/Navbar/Navbar.jsx
+++ medisearch-frontend/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "./Navbar.css";
-import { useNavigate } from 'react-router-dom';
-import { loginUser, logoutUser } from '../../Actions/UserActions';
+import { useNavigate } from "react-router-dom";
+import { loginUser, logoutUser } from "../../Actions/UserActions";
 import { useDispatch, useSelector } from "react-redux";
-
-
 
 function Navbar() {
   const navigate = useNavigate();
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const dispatch = useDispatch();
   const { isAuthenticated, loading } = useSelector((state) => state.user);
-  
+
   // Function to handle logout
   const handleSignOutClick = () => {
     dispatch(logoutUser()); // Dispatch the logout action
-    alert("Logged Out")
+    alert("Logged Out");
     // window.location.reload();
     window.location.reload();
-    navigate('/login');
+    navigate("/login");
   };
 
   useEffect(() => {
@@ -36,15 +34,18 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-      <div className="navbar-brand">
-        {windowWidth >= 550 && "Medic"}
-        <img
-          className="navbar-logo"
-          src="https://i.imgur.com/V7hGxsI.png"
-          alt="logo"
-        ></img>
-        {windowWidth >= 550 && "Mate"}
-      </div>
+      <a href="/" style={{ textDecoration: "none", color: "inherit" }}>
+        <div className="navbar-brand">
+          {windowWidth >= 550 && "Medic"}
+
+          <img
+            className="navbar-logo"
+            src="https://i.imgur.com/V7hGxsI.png"
+            alt="logo"
+          ></img>
+          {windowWidth >= 550 && "Mate"}
+        </div>
+      </a>
 
       <ul className="navbar-links">
         <li>
@@ -57,21 +58,23 @@ function Navbar() {
           <a href="#a">{windowWidth < 467 ? "About" : "About Us"}</a>
         </li>
         {!isAuthenticated && (
-        <li>
-          <a href="/join">Sign-Up</a>
-        </li>
+          <li>
+            <a href="/signup">Sign-Up</a>
+          </li>
         )}
       </ul>
       <div className="navbar-buttons">
         {!isAuthenticated ? (
           <div>
             <a href="/login" className="login-button">
-          Login
-        </a>
+              Login
+            </a>
           </div>
         ) : (
           <div>
-            <button className="login-button" onClick={handleSignOutClick}>Log Out</button>
+            <button className="login-button" onClick={handleSignOutClick}>
+              Log Out
+            </button>
           </div>
         )}
       </div>
