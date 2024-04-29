147c7b57dc10adc6964e2ea0c9d0ab816b707d99
yashharne <yashharne.ed@gmail.com>
2024-04-30 03:16:28 +0530

    prescription changes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,4 @@
-export const llm_url =
-  "https://baa1-2409-40c2-205e-868-68d1-170f-e9a2-bf93.ngrok-free.app";
+export const llm_url = "https://df72-103-97-165-190.ngrok-free.app";
 
 export const bbackend_url =
   "https://e4d2-2409-40c2-1162-f6f5-a5eb-d093-d289-f40a.ngrok-free.app";
